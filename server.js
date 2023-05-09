f8379da651ac86a3afa201d819c8f9a16f4b72ee
Sir-Lancelot27 <whitleylance@gmail.com>
2023-05-09 00:41:55 -0400

    Base structure of server

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 import minimist from 'minimist';
-import { rpsls } from "../lib/rpsls.js"
+import { rpsls } from "./lib/rpsls.js"
 import express from 'express';
 
 // cmd line arguments
 var args = minimist(process.argv.slice(2));
 
 // Create express application
-const app = express();
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Set port to cmd arg or default of 5000
 const port = arg.port || 5000;
+
+
+
+
+// Default route with /app
+app.get('/app', (req, res) => {
+	res.status(200).send("200 OK");
+});
+
+
+// Default rooute for any endpoints not defined
+app.get("*", function (req, res) {
+    res.status(404).send('404 NOT FOUND');
+});
+
+// Set app to listen on port
+app.listen(port, () => {
+    console.log('Server running on port ${port}');
+});
